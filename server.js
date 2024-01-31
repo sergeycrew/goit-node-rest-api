9b37d3988b6dc23aab0e126bd036560980d265ff
Sergey Crew <sergeycrew.bs@gmail.com>
2024-01-31 13:29:07 -0800

    render.com connected

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-const { DB_HOST } = process.env;
+//const { DB_HOST } = process.env;
+const DB_HOST = require("./config");
 
 mongoose.set("strictQuery", true);
 
