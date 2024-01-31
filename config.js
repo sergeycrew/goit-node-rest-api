f6b7789e11fe9e6d62243ce493cd91ac480ad2fb
Sergey Crew <sergeycrew.bs@gmail.com>
2024-01-31 13:09:25 -0800

    homework created

---
+++ config.js
@@ -0,0 +1,7 @@
+const USERNAME = "SRG";
+const PASSWORD = 3332807776;
+const DATA_BASE = "db-contacts";
+
+const DB_HOST = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.qi2zsie.mongodb.net/${DATA_BASE}`;
+
+module.exports = DB_HOST;
